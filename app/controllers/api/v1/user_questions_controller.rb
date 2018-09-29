6707e97739dad17b27eda43914ac316c73e7f517
github username <github email address>
2018-09-29 11:44:53 -0400

    added a create action for users questions controller

--- app/controllers/api/v1/user_questions_controller.rb
+++ app/controllers/api/v1/user_questions_controller.rb
@@ -1,4 +1,4 @@
-class UserQuestionsController < ApplicationController
+class Api::V1::UserQuestionsController < ApplicationController
 
   def index
     @userquestions = UserQuestion.all
@@ -8,6 +8,21 @@ class UserQuestionsController < ApplicationController
   def show
     @userquestion = UserQuestion.find(params[:id])
     render json: @userquestion
+  end
+
+  def create
+    @userquestion = UserQuestion.new(user_question_params)
+    if @userquestion.save
+      render json: @userquestion, status: :accepted
+    else
+      render json: {errors: @userquestion.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def user_question_params
+    params.require(:user_question).permit(:user_id, :question_id, :answeredCorrectly)
   end
 
 end
